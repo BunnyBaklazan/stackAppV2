a866f3c22188047f9bbad18290555fbd3a78ae3a
eamon9 <eamon9@icloud.com>
2022-09-02 15:26:34 +0300

    Update
    Sample files

--- src/main/java/com/example/stackapp/pages/App.java
+++ src/main/java/com/example/stackapp/pages/App.java
@@ -2,6 +2,7 @@ package com.example.stackapp.pages;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,14 +11,17 @@ import java.io.IOException;
 import static java.util.Objects.requireNonNull;
 
 public class App extends Application {
+
+    private static final String PAGE = "columns_page.fxml";
+    private static Stage window;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader= new FXMLLoader(App.class.getResource("view.fxml"));
-        Scene scene= new Scene(fxmlLoader.load(), 900, 700);
+        window= stage;
+        Parent root = FXMLLoader.load(requireNonNull(getClass().getResource(PAGE)));
+        window.setScene(new Scene(root));
 
         stage.setTitle("Hello StackAPP");
         stage.setResizable(false);
-        stage.setScene(scene);
         stage.show();
     }
 
