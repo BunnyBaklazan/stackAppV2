5203fc7c175132c649ba956a43d352cdc71b1062
akovalevskaia <kovalevskaia.av@gmail.com>
2022-09-02 15:21:04 +0300

    Connection login form to DB

--- src/main/java/com/example/stackapp/controller/LoginController.java
+++ src/main/java/com/example/stackapp/controller/LoginController.java
@@ -8,15 +8,13 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
+import java.sql.*;
 
 public class LoginController {
 
     private static final String MAIN_PAGE = "main.fxml";
     private static final String SIGNUP_PAGE = "pages/sign-up.fxml";
     private static final Main window = new Main();
-
-    private static final String TEST_USERNAME = "oleg";
-    private static final String TEST_PASSWORD = "123";
 
     @FXML
     private Button button_login;
@@ -41,21 +39,52 @@ public class LoginController {
         window.changePage(SIGNUP_PAGE);
     }
 
-    public void getMeCredentials() throws IOException {
+    public void login() throws IOException {
         String username = tf_username.getText();
         String password = pf_password.getText();
 
         System.out.println("USER ENTERED VARIABLED " + username + " " + password);
         /// CONNECT TO DABASE, RECEIVE INFO
-
-        if (username.equals(TEST_USERNAME) && password.equals(TEST_PASSWORD)) {
-            System.out.println("SUCCESFULL");
-            window.changePage(SIGNUP_PAGE);
-        } else {
-            System.out.println("BAD CREDENTIALS");
-            window.changePage(MAIN_PAGE);
+        if (username.trim().isEmpty() || password.trim().isEmpty()) {
+            System.out.println("Username or password is missing");
+            l_errorText.setVisible(true);
+            return;
         }
+        l_errorText.setVisible(false);
+        // connect to DB
+        Connection conn = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:stackAppdbv1.db";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+            preparedStatement = conn.prepareStatement("SELECT password FROM users WHERE USERNAME = ?");
+            preparedStatement.setString(1, username);
+            resultSet = preparedStatement.executeQuery();
 
+            if (!resultSet.isBeforeFirst()) {
+                System.out.println("User is not found!");
+                l_errorText.setText("User is not found!");
+                l_errorText.setVisible(true);
+            } else {
+                while (resultSet.next()) {
+                    String retrievedPassword = resultSet.getString("password");
+                    if (retrievedPassword.equals(password)) {
+                        System.out.println("User found and password is correct.");
+                        //window.changePage(SIGNUP_PAGE);
+                    } else {
+                        System.out.println("Passwords did not match!");
+                        l_errorText.setText("Passwords did not match!");
+                        l_errorText.setVisible(true);
+                        //window.changePage(MAIN_PAGE);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Exception!");
+        }
     }
 
 }
