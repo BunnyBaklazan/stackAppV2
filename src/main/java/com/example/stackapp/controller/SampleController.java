5f43f57812c91f3dcaf2913774cd630eb14be287
eamon9 <eamon9@icloud.com>
2022-09-11 02:40:36 +0300

    Updates
    SampleController change Label to AdminPage works fine, but Update function doesnt work...

--- src/main/java/com/example/stackapp/controller/SampleController.java
+++ src/main/java/com/example/stackapp/controller/SampleController.java
@@ -823,6 +823,11 @@ public class SampleController {
     }
 
     @FXML
+    private void checkIfErrorIsOn() {
+        label_error.setText("");
+    }
+
+    @FXML
     private void insert() {
         UserData user = new UserData(
                 tf_first_name.getText(),
