9b46a76c9d052398bbc9c41ed1bb11c5650184d9
eamon9 <eamon9@icloud.com>
2022-09-11 02:18:22 +0300

    SignIn and css updates I adapted the SignIn design to our overall design. Fixed the SignIn function to show errors correctly

--- src/main/java/com/example/stackapp/controller/LoginController.java
+++ src/main/java/com/example/stackapp/controller/LoginController.java
@@ -3,7 +3,6 @@ package com.example.stackapp.controller;
 import com.example.stackapp.Main;
 import connect.net.sqlite.Connect;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import org.mindrot.jbcrypt.BCrypt;
@@ -13,7 +12,6 @@ import java.sql.*;
 import java.util.prefs.Preferences;
 
 public class LoginController {
-    private static final String MAIN_PAGE = "main.fxml";
     private static final String SAMPLE_PAGE = "pages/sample.fxml";
     private static final Preferences userPreferences = Preferences.userRoot();
     private static final Main window = new Main();
@@ -25,37 +23,32 @@ public class LoginController {
     private PasswordField pf_password;
 
     @FXML
-    private Label l_errorText;
-
-    public void backToMainPage() throws IOException {
-        window.changePage(MAIN_PAGE);
-    }
+    private TextField errorTxtField;
 
     public static boolean isValidCredentials(String username, String password) {
         return !username.trim().isEmpty() && !password.trim().isEmpty();
     }
 
+    @FXML
     public void login() {
         String username = tf_username.getText();
         String password = pf_password.getText();
         ResultSet result = null;
 
-        System.out.println("USER ENTERED VARIABLED " + username + " " + password);
-
         if (!isValidCredentials(username, password)) {
             System.out.println("Username or password is missing");
-            l_errorText.setVisible(true);
+            errorTxtField.setVisible(true);
+            errorTxtField.setText("Username or password is missing!");
             return;
         }
-        l_errorText.setVisible(false);
+        errorTxtField.setVisible(false);
 
         try {
             result = Connect.checkLogin(username);
 
             if (!result.isBeforeFirst()) {
-                System.out.println("User is not found!");
-                l_errorText.setText("User is not found!");
-                l_errorText.setVisible(true);
+                errorTxtField.setText("User is not found!");
+                errorTxtField.setVisible(true);
 
             } else {
                 while (result.next()) {
@@ -69,17 +62,13 @@ public class LoginController {
                         userPreferences.put("username", username);
                         if (username.equals("asd.asd")) { //need to change to "admin"
                             userPreferences.put("role", "admin");
-                            window.changePage(SAMPLE_PAGE);
                         } else {
                             userPreferences.put("role", "worker");
-                            window.changePage(SAMPLE_PAGE);
                         }
-
+                        window.changePage(SAMPLE_PAGE);
                     } else {
-                        System.out.println("Passwords did not match!");
-                        l_errorText.setText("Passwords did not match!");
-                        l_errorText.setVisible(true);
-
+                        errorTxtField.setText("Passwords did not match!");
+                        errorTxtField.setVisible(true);
                     }
                 }
             }
